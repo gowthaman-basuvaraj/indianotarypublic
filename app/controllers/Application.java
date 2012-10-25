e54a7e72bcf5ea6d00f182a1ff6a892d47e281a4
Gowthaman B <gowthaman@365build.in>
2012-10-25 08:08:13 +0530

    init commit
    Signed-off-by: Gowthaman B <gowthaman@365build.in>

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import jobs.FetchJob;
 import play.*;
 import play.mvc.*;
 
@@ -9,8 +10,51 @@ import models.*;
 
 public class Application extends Controller {
 
-    public static void index() {
-        render();
+    public static void index(String district, String state) {
+        String[] states = new String[]{
+                "Andhra Pradesh",
+                "Arunachal Pradesh",
+                "Assam",
+                "Bihar",
+                "Chhattisgarh",
+                "Goa",
+                "Gujarat",
+                "Haryana",
+                "Himachal Pradesh",
+                "Jammu and Kashmir",
+                "Jharkhand",
+                "Karnataka",
+                "Kerala",
+                "Madhya Pradesh",
+                "Maharashtra",
+                "Manipur",
+                "Meghalaya",
+                "Mizoram",
+                "Nagaland",
+                "Odisha",
+                "Punjab",
+                "Rajasthan",
+                "Sikkim",
+                "Tamil Nadu",
+                "Tripura",
+                "Uttarakhand",
+                "Uttar Pradesh",
+                "West Bengal",
+
+                "Andaman and Nicobar Islands",
+                "Chandigarh",
+                "The Government of NCT of Delhi",
+                "Dadra and Nagar Haveli",
+                "Daman and Diu",
+                "Lakshadweep",
+                "Puducherry",
+
+        };
+        render(district, state);
     }
 
+    public static void fetch() throws Exception {
+        new FetchJob().doJob();
+        renderText("OK");
+    }
 }
--- app/jobs/FetchJob.java
+++ app/jobs/FetchJob.java
@@ -25,7 +25,7 @@ import java.util.regex.Pattern;
  * Time: 9:46 PM
  * To change this template use File | Settings | File Templates.
  */
-@OnApplicationStart
+//@OnApplicationStart
 public class FetchJob extends Job {
     @Override
     public void doJob() throws Exception {
@@ -33,8 +33,8 @@ public class FetchJob extends Job {
         //http://lawmin.nic.in/la/notarylist1.htm
         //http://lawmin.nic.in/la/notarylist2.htm
         fetch("http://lawmin.nic.in/la/notarylist3.htm");
-        //fetch("http://lawmin.nic.in/la/notarylist1.htm");
-        //fetch("http://lawmin.nic.in/la/notarylist2.htm");
+        fetch("http://lawmin.nic.in/la/notarylist1.htm");
+        fetch("http://lawmin.nic.in/la/notarylist2.htm");
     }
 
     public void fetch(String url) {
